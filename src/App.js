8457a659a723e38ffe2cfb002664382e7c01bb60
kimitexpress <142508899+kimitexpress@users.noreply.github.com>
2024-01-21 22:24:17 +0200

    uploading the whole project for the first time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [enableNegative, setEnableNegative] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div>
+        <button
+          style={{
+            cursor: "pointer",
+            fontSize: "20px",
+            border: "solid 1px white",
+            borderRadius: "5px",
+            padding: "12px 24px",
+            textAlign: "center",
+            margin: "20px",
+          }}
+          onClick={() => {
+            enableNegative
+              ? setCounter(() => counter - 1)
+              : setCounter(() => (counter <= 0 ? 0 : counter - 1));
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Decrease
+        </button>
+        <button
+          style={{
+            cursor: "pointer",
+            fontSize: "20px",
+            border: "solid 1px white",
+            borderRadius: "5px",
+            padding: "12px 24px",
+            textAlign: "center",
+            margin: "20px",
+          }}
+          onClick={() => setCounter(() => counter + 1)}
+        >
+          Increase
+        </button>
+      </div>
+
+      <h1 style={{ fontSize: "50px", margin: "30px" }}>{counter}</h1>
+      <h1>
+        {enableNegative
+          ? "Negative value is enabled"
+          : "Negative value is disabled"}
+      </h1>
+      <div
+        style={{
+          marginTop: "30px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <input
+          name="value"
+          id="value"
+          type="checkbox"
+          style={{
+            height: "20px",
+            width: "20px",
+            marginRight: "10px",
+            borderRadius: "10px",
+          }}
+          checked={enableNegative}
+          onChange={() => {
+            setEnableNegative(!enableNegative);
+          }}
+        />
+        <label htmlFor="value" style={{ fontSize: "20px" }}>
+          Enable negative value
+        </label>
+      </div>
+      <div style={{ marginTop: "20px" }}>
+        {!enableNegative && counter < 0 ? (
+          <h3
+            style={{
+              fontWeight: "300",
+              color: "#fe7968 ",
+              fontSize: "16px",
+            }}
+          >
+            NOTE: You Will lose the current value if you decrease, to continue
+            enable negative value!
+          </h3>
+        ) : (
+          <h3> </h3>
+        )}
+      </div>
     </div>
   );
 }
